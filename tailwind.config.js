0d091e7347f44fb025f623733de1f5d266eaf366
Sofus Addington <sofus@addington.dk>
2024-03-22 14:56:01 +0100

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.html"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};
