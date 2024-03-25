e44bc4b0904025a29dff550f22db82f64ec2072f
Sofus Addington <sofus@addington.dk>
2024-03-25 21:25:49 +0100

    Frontpage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  darkMode: "media",
   content: ["./src/**/*.html"],
   future: {
     hoverOnlyWhenSupported: true,
